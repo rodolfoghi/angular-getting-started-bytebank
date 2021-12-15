fd3a36919f88ff981851f57bb91fd1a496048c06
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2021-12-14 22:05:41 -0300

    Criando o evento do formulário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {  NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NovaTransferenciaComponent
-  ],
-  imports: [
-    BrowserModule,
-  ],
+  declarations: [AppComponent, NovaTransferenciaComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
