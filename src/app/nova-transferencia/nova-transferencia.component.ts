e9a7d5c7751a5b3746a28130c3e80f2a571eadf3
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2021-12-14 22:14:36 -0300

    Property Bind

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  valor: number;
+  destino: number;
+
   transferir() {
-    console.log('Solicitada nova transferência');
+    console.log('valor:', this.valor);
+    console.log('destino:', this.destino);
   }
 }
